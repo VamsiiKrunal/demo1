d275229ddddcd764f0d0ebe9f3dd17c2c19a2d83
VamsiiKrunal <vamsiiguttikonda@gmail.com>
2024-07-09 16:41:28 +0530

    excluded node modules

--- src/A.js
+++ src/A.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import Conditional from './Conditional'
 import B from './B';
-import Car from './Car'
+const UserContext=React.createContext()
 const A = () => {
-
+ const [name, setname] = useState("krishna")
+let a=10;
   return (
     <div>
-      <Car/>
+      
+      <UserContext.Provider value={{name,setname}}>
+        <Car/>
+      <B/>
+      </UserContext.Provider>
     </div>
   )
 }
 export default A
+export {UserContext}
